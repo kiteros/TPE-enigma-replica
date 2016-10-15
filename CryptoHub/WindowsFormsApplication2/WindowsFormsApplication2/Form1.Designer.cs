d6127a15ae6a55f8d311c09030b71dd9fec02ab7
Jules <jules.eschbach@icloud.com>
2016-10-15 13:18:56 +0200

    Add files via upload

--- CryptoHub/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ CryptoHub/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -39,29 +39,29 @@
             this.label2 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
-            this.label16 = new System.Windows.Forms.Label();
-            this.label17 = new System.Windows.Forms.Label();
-            this.label18 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -94,7 +94,7 @@
             this.textBox1.Location = new System.Drawing.Point(9, 32);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 214);
+            this.textBox1.Size = new System.Drawing.Size(291, 214);
             this.textBox1.TabIndex = 2;
             // 
             // label1
@@ -171,6 +171,15 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Message codé";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Passage a la lettre suivante";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -225,6 +234,142 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Paramètres";
             // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(123, 214);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(41, 13);
+            this.label18.TabIndex = 22;
+            this.label18.Text = "label18";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(123, 201);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 13);
+            this.label17.TabIndex = 21;
+            this.label17.Text = "label17";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(123, 188);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(41, 13);
+            this.label16.TabIndex = 20;
+            this.label16.Text = "label16";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(123, 176);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "label15";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(123, 163);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "label14";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(123, 150);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "label13";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(123, 137);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "label12";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(123, 124);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "label11";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(123, 111);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(123, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "label9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(123, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "label8";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(120, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Aucune inversion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Inversion des lettres :";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(294, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Modifier le cablage des rotors";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(293, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Modifier le plugboard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
@@ -253,151 +398,6 @@
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(338, 221);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(198, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Passage a la lettre suivante";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(7, 259);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(293, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Modifier le plugboard";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(6, 288);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(294, 23);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Modifier le cablage des rotors";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 72);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(107, 13);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Inversion des lettres :";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(120, 72);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(89, 13);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Aucune inversion";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(123, 85);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "label8";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(123, 98);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "label9";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(123, 111);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(41, 13);
-            this.label10.TabIndex = 14;
-            this.label10.Text = "label10";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(123, 124);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(41, 13);
-            this.label11.TabIndex = 15;
-            this.label11.Text = "label11";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(123, 137);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(41, 13);
-            this.label12.TabIndex = 16;
-            this.label12.Text = "label12";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(123, 150);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(41, 13);
-            this.label13.TabIndex = 17;
-            this.label13.Text = "label13";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(123, 163);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(41, 13);
-            this.label14.TabIndex = 18;
-            this.label14.Text = "label14";
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(123, 176);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(41, 13);
-            this.label15.TabIndex = 19;
-            this.label15.Text = "label15";
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(123, 188);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(41, 13);
-            this.label16.TabIndex = 20;
-            this.label16.Text = "label16";
-            // 
-            // label17
-            // 
-            this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(123, 201);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(41, 13);
-            this.label17.TabIndex = 21;
-            this.label17.Text = "label17";
-            // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(123, 214);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(41, 13);
-            this.label18.TabIndex = 22;
-            this.label18.Text = "label18";
             // 
             // Form1
             // 
--- CryptoHub/WindowsFormsApplication2/WindowsFormsApplication2/plugboard.cs
+++ CryptoHub/WindowsFormsApplication2/WindowsFormsApplication2/plugboard.cs
@@ -12,14 +12,258 @@ namespace WindowsFormsApplication2
 {
     public partial class plugboard : Form
     {
+        private bool[] clicked = new bool[40];
+        private bool[] isBloked = new bool[40];
+        private CirclePanel[] panels = new CirclePanel[40];
+        private Label[] lettresText = new Label[40];
+        private Label[] liaisonsOk = new Label[20];
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private int nbCLicksAffile = 0;
+        private int nbLignesOk = 0;
+        private char premLettre;
+        private char secLettre;
+        private Point dep;
+        List<Ligne> touteLignes = new List<Ligne>();
+
         public plugboard()
         {
             InitializeComponent();
+
+            for (int x = 0; x < 20; x++)
+            {
+                panels[x] = new CirclePanel();
+                panels[x].Location = new Point(10 + 55 * x, 60);
+                panels[x].Size = new Size(50, 50);
+                panels[x].Name = x.ToString();
+                //panels[x].BackColor = Color.Black;
+                panels[x].MouseEnter += new EventHandler(pan_MouseEnter);
+                panels[x].MouseLeave += new EventHandler(pan_MouseLeave);
+                panels[x].Click += new EventHandler(pan_Click);
+
+                this.Controls.Add(panels[x]);
+                lettresText[x] = new Label();
+                lettresText[x].Location = new Point(10 + 55 * x, 5);
+                lettresText[x].Text = alphabet[x].ToString();
+                lettresText[x].AutoSize = true;
+                lettresText[x].Font = new Font(lettresText[x].Font.FontFamily, 35);
+                this.Controls.Add(lettresText[x]);
+                clicked[x] = false;
+            }
+            for (int x = 20; x < 40; x++)
+            {
+                panels[x] = new CirclePanel();
+                panels[x].Location = new Point(10 + 55 * (x - 20), 300);
+                panels[x].Size = new Size(50, 50);
+                panels[x].Name = x.ToString();
+                //panels[x].BackColor = Color.Black;
+
+                panels[x].MouseEnter += new EventHandler(pan_MouseEnter);
+                panels[x].MouseLeave += new EventHandler(pan_MouseLeave);
+                panels[x].Click += new EventHandler(pan_Click);
+
+                this.Controls.Add(panels[x]);
+                lettresText[x] = new Label();
+                lettresText[x].Location = new Point(10 + 55 * (x - 20), 350);
+                lettresText[x].Text = alphabet[x - 20].ToString();
+                lettresText[x].AutoSize = true;
+                lettresText[x].Font = new Font(lettresText[x].Font.FontFamily, 35);
+                this.Controls.Add(lettresText[x]);
+                clicked[x] = false;
+            }
+            panel1.Parent.MouseMove += new MouseEventHandler(pan_MouseMove);
+
         }
 
+        private void pan_MouseEnter(object sender, EventArgs e)
+        {
+            string PanelName = ((CirclePanel)sender).Name;
+            int valueOfPanel = Int32.Parse(PanelName);
+            //panels[valueOfPanel].BackColor = Color.Black;
+            if (!clicked[valueOfPanel] && !isBloked[valueOfPanel])
+            {
+                panels[valueOfPanel].hoverEllipse();
+            }
+        }
+
+        private void pan_MouseLeave(object sender, EventArgs e)
+        {
+            string PanelName = ((CirclePanel)sender).Name;
+            int valueOfPanel = Int32.Parse(PanelName);
+            //panels[valueOfPanel].BackColor = Color.Transparent;
+            if (!clicked[valueOfPanel] && !isBloked[valueOfPanel])
+            {
+                panels[valueOfPanel].clearEllipse();
+            }
+
+        }
+
+        private void pan_Click(object sender, EventArgs e)
+        {
+            string PanelName = ((CirclePanel)sender).Name;
+            int valueOfPanel = Int32.Parse(PanelName);
+            if (!isBloked[valueOfPanel])
+            {
+                panels[valueOfPanel].fillEllipse();
+                clicked[valueOfPanel] = true;
+                if (nbCLicksAffile == 0)
+                {
+                    premLettre = alphabet[valueOfPanel % 20];
+                }
+                else
+                {
+                    secLettre = alphabet[valueOfPanel % 20];
+                }
+
+                nbCLicksAffile++;
+                if (nbCLicksAffile == 2)
+                {
+                    //Nouvelle liaison
+                    nbCLicksAffile = 0;
+                    panels[alphabet.IndexOf(premLettre) + 20].WrongEllipse();
+                    isBloked[alphabet.IndexOf(premLettre) + 20] = true;
+                    panels[alphabet.IndexOf(secLettre)].WrongEllipse();
+                    isBloked[alphabet.IndexOf(secLettre)] = true;
+                    Point t = this.PointToClient(Cursor.Position);
+                    Ligne ligne = new Ligne(dep, t);
+                    touteLignes.Add(ligne);
+
+                    liaisonsOk[nbLignesOk] = new Label();
+                    liaisonsOk[nbLignesOk].Location = new Point(10, 450 + nbLignesOk * 15);
+                    liaisonsOk[nbLignesOk].AutoSize = true;
+                    liaisonsOk[nbLignesOk].Font = new Font(liaisonsOk[nbLignesOk].Font.FontFamily, 10);
+                    liaisonsOk[nbLignesOk].Text = premLettre.ToString() + "-" + secLettre.ToString();
+                    this.Controls.Add(liaisonsOk[nbLignesOk]);
+                    nbLignesOk++;
+                }
+
+                dep = ((CirclePanel)sender).Location;
+                dep.X += 25;
+                dep.Y += 25;
+            }
+        }
+
+        private void dessinerLigne(Point depart, Point finish, List<Ligne> l)
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            graphicsObj.Clear(Color.White);
+            Pen blackPen = new Pen(Color.Black, 3);
+            graphicsObj.DrawLine(blackPen, depart, finish);
+            if (l.Any())
+            {
+                for(int y = 0; y < l.Count; y++)
+                {
+                    graphicsObj.DrawLine(blackPen, l[y].getDep(), l[y].getArr());
+                }
+            }
+        }
+
+        private void pan_MouseMove(object sender, EventArgs e)
+        {
+            if (nbCLicksAffile == 1)
+            {
+                Point p = this.PointToClient(Cursor.Position);
+                dessinerLigne(dep, p, touteLignes);
+            }
+
+        }
+
+        //Entrée 1
+        /*
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            pictureBox1.BackColor = Color.Black;
+            pictureBox1.BackColor = Color.DarkBlue;
+            clicked[1] = !clicked[1];
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            if (!clicked[1])
+            {
+                pictureBox1.BackColor = Color.Orange;
+            }
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            if (!clicked[1])
+            {
+                pictureBox1.BackColor = Color.White;
+            }
+            
+        }*/
+
+    }
+    public class CirclePanel : Panel
+    {
+        public CirclePanel()
+        {
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawEllipse(Pens.Black, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            this.Width = this.Height;
+            base.OnResize(e);
+        }
+
+        public void fillEllipse()
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.Blue);
+            graphicsObj.FillEllipse(myBrush, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        public void WrongEllipse()
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            graphicsObj.FillEllipse(myBrush, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        public void clearEllipse()
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            graphicsObj.FillEllipse(myBrush, 0, 0, this.Width - 1, this.Height - 1);
+            graphicsObj.DrawEllipse(Pens.Black, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        public void hoverEllipse()
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.Orange);
+            graphicsObj.FillEllipse(myBrush, 0, 0, this.Width - 1, this.Height - 1);
+        }
+    }
+
+    public class Ligne
+    {
+        private Point dep_;
+        private Point Arr_;
+        public Ligne(Point dep, Point Arr)
+        {
+            this.dep_ = dep;
+            this.Arr_ = Arr;
+        }
+
+        public Point getDep()
+        {
+            return dep_;
+        }
+
+        public Point getArr()
+        {
+            return Arr_;
         }
     }
 }
