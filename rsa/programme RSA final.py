2849f833428f6d14c4cc71966fb942989784ea24
pixover <Pixover.83@gmail.com>
2016-11-23 12:02:59 +0100

    Update programme RSA final.py

--- rsa/programme RSA final.py
+++ rsa/programme RSA final.py
@@ -1,14 +1,29 @@
 from tkinter import *
+from time import *
+
+def close():
+    nbprems.destroy()
+    
 
 fenetre=Tk()
 fenetre.geometry("1080x720")
+
+
 titre=Label(fenetre, text='Programme RSA')
 titre.pack(pady=7)
-nbprems=Button(fenetre, text='Nombres premiers')
+nbprems=Button(fenetre, text='Nombres premiers', command=close)
 nbprems.pack(pady=5)
 chiffrementRsa=Button(fenetre, text='Chiffrement RSA')
 chiffrementRsa.pack(pady=5)
 dechiffrementRsa=Button(fenetre, text='Déchiffrement RSA')
 dechiffrementRsa.pack(pady=5)
 
-fenetre.mainloop()
+
+
+
+
+
+
+
+fenetre.mainloop
+
